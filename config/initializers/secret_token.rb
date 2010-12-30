18da35380f2bcac1426007011d698fd9bfb5b6da
Bhaarat Sharma <bhaarat.s@gmail.com>
2010-12-30 10:04:53 -0500

    add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordsearch::Application.config.secret_token = '8939966dda4ae6b53818ac011afa40ca6c97861711ffed5619a27ba5559f90e5865301edc6e94f464303cbec26dc8fdbd99217ca0eb75797c65b25d5840b4323'
